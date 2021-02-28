b25f24a2adc6eb9e4a8d6e84e4c2fc625afb263a
amir-lahav <amir.lahav@shutterfly.com>
2021-02-28 16:55:27 +0200

    main

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "LookKitSDK",
-            targets: ["LookKitSDK"])
+            name: "LookKit",
+            targets: ["LookKit"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,7 +18,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "LookKitSDK", path: "./Sources/LookKitSDK.xcframework")
+        .binaryTarget(name: "LookKit", path: "./Sources/LookKit.xcframework")
 
     ]
 )
