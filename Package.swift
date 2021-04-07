e9c29f183da5f858c4c06b349d350b8ba83a69ce
la-labs <lalabsproducts@gmail.com>
2021-04-07 22:43:36 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "LookKit_Package",
+    name: "LookKit",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
